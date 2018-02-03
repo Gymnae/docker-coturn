2af28fc2c7ae3f5457da2ce1011fb6818ca56c36
Gymnae <dagorwaith@gmail.com>
2018-02-03 13:58:55 +0000

    first commit

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM gymnae/alpine-base:latest
+LABEL maintainer="docker@grundstil.de" \
+      tag="coturn"
+
+RUN apk add --no-cache --update bash coturn@community curl gettext jq
+
+
+ADD coturn.sh /coturn.sh
+RUN chmod u+x /coturn.sh
+
+EXPOSE 3389 3389/udp 53 53/udp 8080 80 8080/udp 3478 3479 3479/udp 3478/udp 443 443/udp 5349 5349/udp  
+
+CMD /coturn.sh
---
+++ coturn.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+# Discover public and private IP for this instance
+PUBLIC_IPV4="$(curl -4 icanhazip.com)"
+PRIVATE_IPV4="$(ip addr show eth0 | grep 'inet ' | awk '{print $2}' | cut -d/ -f1)"
+
+PORT=${PORT:-3478}
+ALT_PORT=${PORT:-3479}
+
+TLS_PORT=${TLS:-5349}
+TLS_ALT_PORT=${PORT:-5350}
+
+MIN_PORT=${MIN_PORT:-49152}
+MAX_PORT=${MAX_PORT:-65535}
+
+TURNSERVER_CONFIG=/etc/coturn/turnserver.conf
+
+cat <<EOF > ${TURNSERVER_CONFIG}.default
+# https://github.com/coturn/coturn/blob/master/examples/etc/coturn.conf
+listening-port=${PORT}
+listening-ip=${PUBLIC_IPV4}
+relay-ip=${PUBLIC_IPV4}
+#external-ip=${PUBLIC_IPV4}
+#min-port=${MIN_PORT}
+#max-port=${MAX_PORT}
+fingerprint
+lt-cred-mech
+total-quota=100
+#bps-capacity=6400000
+#max-bps=640000
+stale-nonce
+cipher-list="ECDH+AESGCM:DH+AESGCM:ECDH+AES256:DH+AES256:ECDH+AES128:DH+AES:ECDH+3DES:DH+3DES:RSA+AES:RSA+3DES:!ADH:!AECDH:!MD5"
+no-loopback-peers
+no-multicast-peers
+
+EOF
+
+
+if [ -n "${JSON_CONFIG}" ]; then
+  echo "${JSON_CONFIG}" | jq -r '.config[]' >> ${TURNSERVER_CONFIG}.default
+fi
+
+if [ -n "$STATIC_AUTH_SECRET" ]; then
+  echo "use-auth-secret" >> ${TURNSERVER_CONFIG}.default
+  echo "static-auth-secret=${STATIC_AUTH_SECRET}" >> ${TURNSERVER_CONFIG}.default
+  echo "realm=${REALM}" >> ${TURNSERVER_CONFIG}.default
+fi
+
+if [ -n "$LETSENCRYPT" ]; then
+  cp /certs/${REALM}.crt /etc/coturn/turnserver_cert.pem
+  cp /certs/${REALM}.key /etc/coturn/turnserver_pkey.pem
+
+  cat <<EOT >> ${TURNSERVER_CONFIG}.default
+tls-listening-port=${TLS_PORT}
+alt-tls-listening-port=${TLS_ALT_PORT}
+cert=/etc/coturn/turnserver_cert.pem
+pkey=/etc/coturn/turnserver_pkey.pem
+EOT
+
+fi
+
+envsubst < ${TURNSERVER_CONFIG}.default > ${TURNSERVER_CONFIG}
+
+exec /usr/bin/turnserver -v
